16affdf01820830dbc3c2d5e9996d671d3702ae5
Sean Sidwell <seanwilliamsidwell@gmail.com>
2023-04-12 11:24:04 -0700

    Update Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Form1Component } from './form1/form1.component';``
+import { DateComponent } from './date/date.component';
+
+
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form1Component,
+    DateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MatNativeDateModule, MatInputModule],
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
---
+++ src/app/form1/form1.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-form1',
+  templateUrl: './form1.component.html',
+  styleUrls: ['./form1.component.css']
+})
+export class Form1Component {
+
+  constructor(private formBuilder: FormBuilder){
+    
+  }
+
+}
